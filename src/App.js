ce4185e45b39e27c03319cdb833bae0bc19084c7
moja123 <umarroshidi@yahoo.com>
2024-02-05 18:53:18 +0800

    created MovieCard.jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { useEffect } from 'react'; //import React object from 'react' library
+import React, { useEffect, useState } from 'react'; //import React object from 'react' library
 
 import './App.css';
 import SearchIcon from './search.svg';
+
+import MovieCard from './MovieCard';
 
 //OMDB api key: ec451e8a
 
 const API_URL = 'http://www.omdbapi.com?apikey=' + process.env.REACT_APP_API_KEY;
 
+const movie1 = {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "imdbID": "tt0372784",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+}
+
 //below 'App' variable is whats called a react component
 const App = () => {
+
+    //set useState() hook, the [] part is using destructuring
+    const [movies, setMovies] = useState([]);//we pass in empty array as the states default value
 
     // below is how you implement an api call without any http protocol routing (get/post/put/delete), 
     // you will need express.js for http routing/REST, and usually done from a dedicated backend
@@ -16,11 +29,17 @@ const App = () => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        //console.log(data.Search);
+
+        //never do this:
+        //movies = data.search;
+
+        //instead, state can only be modified using the setState function:
+        setMovies(data.Search);
     }
 
     useEffect(()=>{
-        searchMovies('Spiderman');
+        searchMovies('Batman');
     },[]);
 
     return (
@@ -38,7 +57,12 @@ const App = () => {
                 <img
                     src={SearchIcon}
                     alt="search"
+                    onClick={()=>{}}
                 />
+            </div>
+
+            <div className="container">
+                <MovieCard movie1={movie1} />
             </div>
 
         </div>
